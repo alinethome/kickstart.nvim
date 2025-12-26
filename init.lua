dc3e8a2a573e163ea93c174037edac16b0a98a30
Aline Thome <athomewilliams@gmail.com>
2025-12-26 16:51:15 -0600

    Fix shiftwidth

--- init.lua
+++ init.lua
@@ -162,6 +162,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.autoindent = true
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
